8552678e91fb2af4fc3dde5047c8f3e4c7045dca
ramy walid <ramy.bahy2@gmail.com>
2023-04-06 04:42:17 +0200

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,6 +9,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int j;
 	listint_t *tmp1, *tmp2;
 
 	if (head == NULL || *head == NULL)
@@ -24,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	tmp1 = *head;
-	for (unsigned int j = 0; j < index - 1; j++)
+	for (j = 0; j < index - 1; j++)
 	{
 		if (tmp1 == NULL)
 			return (-1);
