07f6d1d2ac629dc3a563456dcf3c3ea128652a06
ramybahy <ramy.bahy2@gmail.com>
2023-04-07 13:56:36 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-PRINT_BINARY.C
+++ 0x14-bit_manipulation/1-PRINT_BINARY.C
@@ -6,8 +6,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int j, counter = 0;
 	unsigned long int crnt;
+	int j, counter = 0;
 
 	for (j = 63; j >= 0; j--)
 	{
@@ -16,7 +16,7 @@ void print_binary(unsigned long int n)
 		if (crnt & 1)
 		{
 			_putchar('1');
-			crnt++;
+			counter++;
 		}
 		else if (counter)
 			_putchar('0');
