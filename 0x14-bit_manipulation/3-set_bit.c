2ffc6ac603478b1dd1ca2b38181813e3009a333d
ramybahy <ramy.bahy2@gmail.com>
2023-04-08 21:42:35 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,16 +4,16 @@
  * set_bit - start prog
  * @n: evaluate 1st int
  * @index: evaluate 2nd int
- * Description:set bit val to 1
- * Return: return (0) is the required function signature
+ * Description:set bit val to 1 
+ * Return: return req func sig.(0)
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
-return (-1);
-}
-*n ^= (1 << index);
-return (1);
+		return (-1);
+	}
+	*n ^= (1 << index);
+	return (1);
 }
