0e77b8f910054b0a53a70bb2264f0ff026bc309a
ramybahy <ramy.bahy2@gmail.com>
2023-04-10 16:45:21 -0700

    abc

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,8 +15,6 @@ int main(int vfqc, char *vfqb[])
 	if (vfqc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp port rec\n"), exit(97);
 
-	/* open port */
-
 	port = open(vfqb[1], O_RDONLY);
 	if (port == -1)
 	{
@@ -24,13 +22,9 @@ int main(int vfqc, char *vfqb[])
 		exit(98);
 	}
 
-	/* opn file_to */
-
 	rec = open(vfqb[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (rec == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", vfqb[2]), exit(99);
-
-	/* rd from port and type to rec */
 
 	while ((rd_byte = read(port, bfr, 1024)) > 0)
 	{
